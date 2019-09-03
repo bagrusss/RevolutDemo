e2bd620fa904a5c438178c7bb6b083e4e882094e
vladislav <bagrusss@gmail.com>
2019-09-03 22:10:33 +0300

    debug app

--- app/src/main/java/ru/bagrusss/revolutdemo/app/RatesApp.kt
+++ app/src/main/java/ru/bagrusss/revolutdemo/app/RatesApp.kt
@@ -1,32 +1,18 @@
 package ru.bagrusss.revolutdemo.app
 
-import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import ru.bagrusss.revolutdemo.BuildConfig
 import ru.bagrusss.revolutdemo.di.DaggerAppComponent
-import timber.log.Timber
 
 
 /**
  * Created by bagrusss on 13.08.2019
  */
-class RatesApp: DaggerApplication() {
+open class RatesApp : DaggerApplication() {
 
     private val injector by lazy {
         DaggerAppComponent.factory()
                           .create(this)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        Stetho.initializeWithDefaults(this)
-
-        //TODO
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> = injector
